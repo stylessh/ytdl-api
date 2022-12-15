949d979ee49dfe1d7786ae2c5550897f0fefedbf
stylessh <stylessh@gmail.com>
2022-12-15 10:34:37 -0400

    fix: add dist to gitignore

--- dist/routes/video.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-const video_controllers_1 = require("../controllers/video.controllers");
-router.post("/download", video_controllers_1.download);
-exports.default = router;
